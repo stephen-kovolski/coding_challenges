0cf73da014db60b885d04cc855ae0e52a5b62b5a
stephen-kovolski <s.kovolski@gmail.com>
2020-06-10 23:33:23 -0400

    pig latin

--- june2020/pigLatin.js
+++ june2020/pigLatin.js
@@ -1,3 +1,6 @@
+//********************************************************STILL WORKING ON THIS ONE*******************************************************
+
+
 // Pig Latin
 
 // Pig Latin is a way of altering English Words. The rules are as follows:
@@ -93,5 +96,90 @@ function translatePigLatin(str) {
   }
   
   translatePigLatin("consonant");
+
+
+
+
+  const readline = require('readline')
+
+    rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+
+    });
+
+    firstQuestion()
+
+function firstQuestion(answer){
+
+    rl.question('What word would you like translated into Pig Latin?\nString:', (answer) => {
+
+        console.log(answer)
+
+    let newEndWord = '',
+        newBegWord = '',
+        regex = /[aeiou]/;
+
+        if (regex.test(str[0])) {
+            console.log(str+'way')
+        } else {
+
+            for (let i = 1; i < str.length; i++) {
+
+                if (regex.test(str[i])){
+
+                   newBegWord = str.slice(0, i)
+                   newEndWord = str.slice(i, str.length)
+
+                   return console.log(newEndWord + newBegWord + 'ay')
+
+//let newAnswer = '';**************************************************************************
+
+    for (let i = answer.length-1; i >= 0; i--) {
+
+        console.log(answer[i]);
+        
+        newAnswer += answer[i];
+
+    }
+
+    console.log(`\nOriginal: ${answer}\nReversed: ${newAnswer}`);
+
+    rl.question(`Would you like to try again?\n(y/n)\ninput:`, secondQuestion)
+
+
+    })
+
+
+    
+
+}
+
+
+function secondQuestion(answer){
+
+    answer = answer.toLowerCase().substring(0,1);
+
+    if (answer == 'y' || answer == 'yes') {
+        firstQuestion()
+
+    } else {
+        rl.question('rate you experinece 1 t 10 \nRating:', (num) => {
+
+            console.log(`\n\n Thank you for your feedback`);
+            
+
+            rl.close()
+
+        })
+    }
+
+    console.log(answer)
+
+
+
+}
+
+
 
 
